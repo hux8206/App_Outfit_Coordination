752eb7572ebdbdb68578dd981df8d832446b2881
van huu <huunguyen0802@gmail.com>
2026-05-22 17:20:34 +0700

    save name into displayName after create account to display name at profile

--- app/src/main/java/com/example/outfitcoordination/Reponsitory/UserRepository.kt
+++ app/src/main/java/com/example/outfitcoordination/Reponsitory/UserRepository.kt
@@ -3,6 +3,7 @@ package com.example.outfitcoordination.Reponsitory
 import com.example.outfitcoordination.Model.User
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.auth.userProfileChangeRequest
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.tasks.await
 
@@ -14,6 +15,11 @@ class UserRepository {
         return try{
             val result = authen.createUserWithEmailAndPassword(user.email, password).await()
             val userID = result.user?.uid ?: return false //neu uid null return false
+            val firebaseUser = result.user ?: return false
+            val profileUpdates = userProfileChangeRequest {
+                displayName = user.name
+            }
+            firebaseUser.updateProfile(profileUpdates).await()
 
             db.collection("users") //tao document
                 .document(userID)
