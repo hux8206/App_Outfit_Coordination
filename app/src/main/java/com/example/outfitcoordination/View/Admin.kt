085aef7d603cd5412bc4e897aac124440888bb3d
van huu <huunguyen0802@gmail.com>
2026-05-22 17:22:11 +0700

    change way return UI to fragment method, because old way is activity

--- app/src/main/java/com/example/outfitcoordination/View/Admin.kt
+++ app/src/main/java/com/example/outfitcoordination/View/Admin.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.outfitcoordination.R
+import com.example.outfitcoordination.databinding.FragmentAdminBinding
 
 class Admin : Fragment() {
+    private var _binding : FragmentAdminBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+    ): View {
+        _binding = FragmentAdminBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
