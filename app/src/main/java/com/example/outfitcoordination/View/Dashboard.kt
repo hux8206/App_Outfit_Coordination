4829f60dca7d6353122b45f99e27a276ff94fb47
van huu <huunguyen0802@gmail.com>
2026-05-28 17:18:45 +0700

    add bundle to hold data and transfer data from screen to other screen

--- app/src/main/java/com/example/outfitcoordination/View/Dashboard.kt
+++ app/src/main/java/com/example/outfitcoordination/View/Dashboard.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.outfitcoordination.Adapter.ClothesAdapter
+import com.example.outfitcoordination.MainActivity
 import com.example.outfitcoordination.Model.Clothes
-import com.example.outfitcoordination.Reponsitory.ClothesRepository
 import com.example.outfitcoordination.ViewModel.ClothesViewModel
 import com.example.outfitcoordination.databinding.FragmentDashboardBinding
-import com.google.firebase.firestore.FirebaseFirestore
 
 class Dashboard : Fragment() {
     private val viewmodel : ClothesViewModel by viewModels()
@@ -32,7 +30,18 @@ class Dashboard : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        clothesAdapter = ClothesAdapter(clothesList)
+        clothesAdapter = ClothesAdapter(clothesList) {clothes ->
+            val detail = ClothesDetail()
+            val bundle = Bundle() //goi du lieu de chuyen tu man hinh nay sang man hinh khac
+            bundle.putString("image",clothes.image)
+            bundle.putString("female",clothes.female)
+            bundle.putString("male",clothes.male)
+            bundle.putString("name",clothes.name)
+            bundle.putString("color",clothes.color)
+
+            detail.arguments = bundle
+            (requireActivity() as MainActivity).loadFragment(detail)
+        }
 
         binding.rvOutfits.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.rvOutfits.adapter = clothesAdapter
