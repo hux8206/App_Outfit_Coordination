ae07456b1a2ebd0f95d9836dfa8bfd8d07242831
van huu <huunguyen0802@gmail.com>
2026-05-28 17:17:46 +0700

    this is a file which formart clothes name and color to show on screen more beautiful

---
+++ app/src/main/java/com/example/outfitcoordination/DisplayFormart.kt
@@ -0,0 +1,53 @@
+package com.example.outfitcoordination
+
+import android.graphics.Color
+
+object DisplayFormatter {
+    fun clothesName(value: String): String {
+        return when (value) {
+            "ao_thun" -> "Áo thun"
+            "ao_len" -> "Áo len"
+            "so_mi" -> "Sơ mi"
+            "polo" -> "Áo polo"
+            "tank_top" -> "Áo tank top"
+            "crop_top" -> "Áo croptop"
+
+            "ao_khoac" -> "Áo khoác"
+            "ao_chong_nang" -> "Áo chống nắng"
+            "hoodie" -> "Hoodie"
+            "blazer" -> "Blazer"
+            "cardigan" -> "Cardigan"
+            "khong_co" -> "Không có"
+
+            "jeans" -> "Quần jeans"
+            "kaki" -> "Quần kaki"
+            "quan_tay" -> "Quần tây"
+            "quan_ni" -> "Quần nỉ"
+            "quan_ong_suong" -> "Quần ống suông"
+            "short" -> "Quần short"
+            "chan_vay" -> "Chân váy"
+
+            else -> value
+                .replace("_", " ")
+                .replaceFirstChar { it.uppercase() }
+        }
+    }
+
+    fun getColor(value: String): Int {
+        return when (value) {
+            "trang" -> Color.WHITE
+            "den" -> Color.BLACK
+            "xam" -> Color.GRAY
+            "xanh" -> Color.rgb(0, 51, 102)
+            "nau" -> Color.rgb(121, 85, 72)
+            "xanhla" -> Color.rgb(28, 102, 28)
+            "kem" -> Color.rgb(250, 223, 180)
+            "vang" -> Color.YELLOW
+            "hong" -> Color.rgb(255, 192, 203)
+            "do" -> Color.RED
+            "cam" -> Color.rgb(255, 152, 0)
+            "be" -> Color.rgb(232, 224, 213)
+            else -> Color.LTGRAY
+        }
+    }
+}
