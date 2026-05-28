ecaf30aee70e6595f1e76079ef85e0c684d4c9ab
van huu <huunguyen0802@gmail.com>
2026-05-28 20:18:33 +0700

    add filter function

--- app/src/main/java/com/example/outfitcoordination/ViewModel/ClothesViewModel.kt
+++ app/src/main/java/com/example/outfitcoordination/ViewModel/ClothesViewModel.kt
@@ -9,11 +9,36 @@ import com.example.outfitcoordination.Reponsitory.ClothesRepository
 class ClothesViewModel : ViewModel() {
     private val repository = ClothesRepository()
     private val _clothes = MutableLiveData<List<Clothes>>()
+    private val allClothesList = mutableListOf<Clothes>()
     val clothes : LiveData<List<Clothes>>get() = _clothes
 
     fun loadClothes(){
         repository.getClothes { list ->
+            allClothesList.clear()
+            allClothesList.addAll(list)
+
             _clothes.value = list
         }
     }
+
+    fun filterClothes(category : String) {
+        _clothes.value = when (category){
+            "all" -> allClothesList
+
+            "ao_trong" -> allClothesList.filter {
+                it.type in listOf("ao_trong")
+            }
+
+            "ao_khoac" -> allClothesList.filter {
+                it.type in listOf("ao_khoac")
+            }
+
+            "quan" -> allClothesList.filter {
+                it.type in listOf("quan")
+            }
+            else -> allClothesList
+        }
+    }
+
+
 }
