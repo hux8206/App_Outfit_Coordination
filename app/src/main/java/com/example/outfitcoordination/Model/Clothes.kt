f547e6dfb1ea67ccac7aa2c874de49f5bfb80ba0
van huu <huunguyen0802@gmail.com>
2026-05-29 20:11:02 +0700

    change access permission for id, favourite, public to var

--- app/src/main/java/com/example/outfitcoordination/Model/Clothes.kt
+++ app/src/main/java/com/example/outfitcoordination/Model/Clothes.kt
@@ -4,12 +4,12 @@ import android.media.Image
 
 data class Clothes(
     val color : String = "",
-    val favourite : Boolean = false,
-    val public : Boolean = false,
+    var favourite : Boolean = false,
+    var public : Boolean = false,
     val image: String = "",
     val female : String = "",
     val male : String = "",
-    val id : String ="",
+    var id : String ="",
     val type : String = "",
     val name : String = ""
 )
