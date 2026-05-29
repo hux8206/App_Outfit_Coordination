ca996faec6fa4948d097479fadc3fa075bc024da
van huu <huunguyen0802@gmail.com>
2026-05-29 20:12:43 +0700

    add solve click favourite button

--- app/src/main/java/com/example/outfitcoordination/View/Dashboard.kt
+++ app/src/main/java/com/example/outfitcoordination/View/Dashboard.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -15,6 +16,7 @@ import com.example.outfitcoordination.MainActivity
 import com.example.outfitcoordination.Model.Clothes
 import com.example.outfitcoordination.ViewModel.ClothesViewModel
 import com.example.outfitcoordination.databinding.FragmentDashboardBinding
+import kotlinx.coroutines.flow.merge
 
 class Dashboard : Fragment() {
     private val viewmodel : ClothesViewModel by activityViewModels()
@@ -33,17 +35,31 @@ class Dashboard : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        clothesAdapter = ClothesAdapter(clothesList) {clothes ->
-            val detail = ClothesDetail()
-            val bundle = Bundle() //goi du lieu de chuyen tu man hinh nay sang man hinh khac
-            bundle.putString("image",clothes.image)
-            bundle.putString("female",clothes.female)
-            bundle.putString("male",clothes.male)
-            bundle.putString("name",clothes.name)
-            bundle.putString("color",clothes.color)
+        clothesAdapter = ClothesAdapter(
+            list = clothesList,
+            onItemClick = {clothes ->
+                val detail = ClothesDetail()
+                val bundle = Bundle() //goi du lieu de chuyen tu man hinh nay sang man hinh khac
+                bundle.putString("image",clothes.image)
+                bundle.putString("female",clothes.female)
+                bundle.putString("male",clothes.male)
+                bundle.putString("name",clothes.name)
+                bundle.putString("color",clothes.color)
 
-            detail.arguments = bundle
-            (requireActivity() as MainActivity).loadFragment(detail)
+                detail.arguments = bundle
+                (requireActivity() as MainActivity).loadFragment(detail)
+            }
+        ) {clothes ->
+            viewmodel.toggleFavor(clothes){isSuccess->
+                if(isSuccess){
+                    val message = if (clothes.favourite) "Da them vao tu do" else "da xoa khoi tu do"
+                    Toast.makeText(requireContext(),message, Toast.LENGTH_SHORT).show()
+                    clothesAdapter.notifyDataSetChanged()
+                }else{
+                    clothes.favourite = !clothes.favourite
+                    Toast.makeText(requireContext(), "da xay ra loi !!", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
 
         binding.rvOutfits.layoutManager = GridLayoutManager(requireContext(), 2)
