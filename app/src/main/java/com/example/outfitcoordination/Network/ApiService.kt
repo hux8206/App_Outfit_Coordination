b647f088296a14e5656adcac8ab484bd3f6d8ffc
van huu <huunguyen0802@gmail.com>
2026-06-09 21:37:58 +0700

    add endpoint predict_text

--- app/src/main/java/com/example/outfitcoordination/Network/ApiService.kt
+++ app/src/main/java/com/example/outfitcoordination/Network/ApiService.kt
@@ -1,11 +1,15 @@
 package com.example.outfitcoordination.Network
 
-import com.example.outfitcoordination.Model.OutfitResult
-import com.example.outfitcoordination.Model.UserInput
+import com.example.outfitcoordination.Model.ComboBoxInput
+import com.example.outfitcoordination.Model.OutfitsResponse
+import com.example.outfitcoordination.Model.TextInput
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService{
+    @POST("/predict_text")
+    suspend fun predictText(@Body input: TextInput): OutfitsResponse//object UserInput se duoc convert thanh json sau do gui len sever
+
     @POST("/predict")
-    suspend fun predict(@Body input: UserInput): OutfitResult //object UserInput se duoc convert thanh json sau do gui len sever
+    suspend fun predict(@Body input : ComboBoxInput): OutfitsResponse
 }
